b44265dfbf70c07bb6e57d9bfa18af342fa1362e
Praveen Pendyala <praveen@projectyx.com>
2017-04-12 22:26:00 -0700

    Initial Commit

---
+++ DwarfIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DwarfIt
+//
+//  Created by Tech Team on 4/12/17.
+//  Copyright © 2017 Praveen Pendyala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
